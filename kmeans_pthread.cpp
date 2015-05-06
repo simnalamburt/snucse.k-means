78e546e528ce1701426c9a84f8c5f54b6df3fc94
Hyeon Kim <simnalamburt@gmail.com>
2015-05-07 04:14:49 +0900

    Misc change

--- kmeans_pthread.cpp
+++ kmeans_pthread.cpp
@@ -1,15 +1,12 @@
 #include <vector>
 #include <limits>
 #include <thread>
-#include <algorithm>
 #include <cstring>
 #include "kmeans.h"
 
 using std::vector;
 using std::thread;
 using std::numeric_limits;
-using std::for_each;
-using std::fill;
 
 typedef Point point_t;
 
@@ -26,11 +23,9 @@ public:
     for (int i = begin; i < end; ++i) {
       auto min_dist = numeric_limits<float>::max();
       for (int j = 0; j < class_n; ++j) {
-        Point t;
-        t.x = data[i].x - centroids[j].x;
-        t.y = data[i].y - centroids[j].y;
-
-        float dist = t.x * t.x + t.y * t.y;
+        const float x = data[i].x - centroids[j].x;
+        const float y = data[i].y - centroids[j].y;
+        const float dist = x*x + y*y;
         if (dist < min_dist) {
           partitioned[i] = j;
           min_dist = dist;
@@ -40,26 +35,27 @@ public:
   }
 };
 
-void kmeans(int iteration_n, int class_n, int data_n, point_t *centroids, point_t *data, int *partitioned) {
-  vector<int> count(class_n);
+void kmeans(const int iteration_n, const int class_n, const int data_n,
+    point_t *const centroids, point_t *const data, int *const partitioned)
+{
+  const int thread_count = 4;
+  context ctxt(class_n, data_n, centroids, data, partitioned);
 
   for (int _ = 0; _ < iteration_n; ++_) {
-    const int thread_count = 4;
-
-    context ctxt(class_n, data_n, centroids, data, partitioned);
+    // Create threads
     vector<thread> threads;
+    threads.reserve(thread_count);
     for (int t = 0; t < thread_count; ++t) {
-      // Create threads
       threads.emplace_back(&context::per_thread, ctxt, data_n*t/thread_count, data_n*(t+1)/thread_count);
     }
 
     const auto end = threads.cend();
     // Wait for all threads
-    for (auto it = threads.begin(); it != end; ++it) { (*it).join(); }
+    for (auto it = threads.begin(); it != end; ++it) { it->join(); }
 
     // Update step
     memset(centroids, 0, class_n * sizeof *centroids);
-    fill(count.begin(), count.end(), 0);
+    vector<int> count(class_n);
 
     // Calculate mean value
     for (int i = 0; i < data_n; ++i) {
