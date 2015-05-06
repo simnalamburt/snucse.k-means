dd6a0a635251c30ab0ea74333c3c6a452530a5f9
Hyeon Kim <simnalamburt@gmail.com>
2015-05-06 22:54:20 +0900

    멀티스레딩

--- kmeans_pthread.cpp
+++ kmeans_pthread.cpp
@@ -1,20 +1,29 @@
 #include <vector>
 #include <limits>
+#include <thread>
 #include <algorithm>
 #include <cstring>
 #include "kmeans.h"
 
 using std::vector;
+using std::thread;
 using std::numeric_limits;
+using std::for_each;
 using std::fill;
 
-void kmeans(int iteration_n, int class_n, int data_n, Point* centroids, Point* data, int* partitioned) {
-  vector<int> count(class_n);
+typedef Point point_t;
 
-  // Iterate through number of interations
-  for (int _ = 0; _ < iteration_n; ++_) {
-    // Assignment step
-    for (int i = 0; i < data_n; ++i) {
+class context {
+  int class_n, data_n;
+  point_t *centroids, *data;
+  int *partitioned;
+
+public:
+  context(int class_n, int data_n, point_t *centroids, point_t *data, int *partitioned) :
+    class_n(class_n), data_n(data_n), centroids(centroids), data(data), partitioned(partitioned) { }
+
+  void per_thread(int begin, int end) {
+    for (int i = begin; i < end; ++i) {
       auto min_dist = numeric_limits<float>::max();
       for (int j = 0; j < class_n; ++j) {
         Point t;
@@ -28,20 +37,37 @@ void kmeans(int iteration_n, int class_n, int data_n, Point* centroids, Point* d
         }
       }
     }
+  }
+};
+
+void kmeans(int iteration_n, int class_n, int data_n, point_t *centroids, point_t *data, int *partitioned) {
+  vector<int> count(class_n);
+
+  for (int _ = 0; _ < iteration_n; ++_) {
+    const int thread_count = 4;
+
+    context ctxt(class_n, data_n, centroids, data, partitioned);
+    vector<thread> threads;
+    for (int t = 0; t < thread_count; ++t) {
+      // Create threads
+      threads.emplace_back(&context::per_thread, ctxt, data_n*t/thread_count, data_n*(t+1)/thread_count);
+    }
+
+    const auto end = threads.cend();
+    // Wait for all threads
+    for (auto it = threads.begin(); it != end; ++it) { (*it).join(); }
 
     // Update step
-    // Clear sum buffer and class count
     memset(centroids, 0, class_n * sizeof *centroids);
     fill(count.begin(), count.end(), 0);
 
-    // Sum up and count data for each class
+    // Calculate mean value
     for (int i = 0; i < data_n; ++i) {
       centroids[partitioned[i]].x += data[i].x;
       centroids[partitioned[i]].y += data[i].y;
       ++count[partitioned[i]];
     }
 
-    // Divide the sum with number of class for mean point
     for (int i = 0; i < class_n; ++i) {
       centroids[i].x /= count[i];
       centroids[i].y /= count[i];
